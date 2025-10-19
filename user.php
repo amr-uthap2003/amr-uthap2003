cd2237d1ff798168136d1ffa4ff0709b6d3ee40d
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-19 18:12:12 +0530

    Add files via upload

---
+++ user.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'db.php';
+
+class User {
+    private $conn;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function register($username, $email, $password) {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO users (username, email, password) VALUES (:username, :email, :password)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $hashed_password);
+        return $stmt->execute();
+    }
+
+    public function login($email, $password) {
+        $query = "SELECT * FROM users WHERE email=:email LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($user && password_verify($password, $user['password'])) {
+            return $user;
+        }
+        return false;
+    }
+}
+?>
