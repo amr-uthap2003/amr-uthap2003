b077376fc8f0a2232a90c052410a032fbe79dde0
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-20 23:23:57 +0530

    Update cart_page.php

--- cart_page.php
+++ cart_page.php
@@ -1,8 +1,6 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php-error.log');
+
 <?php
 session_start();
 if(!isset($_SESSION['user_id'])) header("Location: login.php");
@@ -129,4 +127,5 @@ document.getElementById('payButton')?.addEventListener('click', function(){
     rzp.open();
 });
 </script>
+
 
