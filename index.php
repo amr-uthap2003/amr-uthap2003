f9814bcf83df945af822edfa58338a17ad4f607a
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-20 22:29:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 session_start();
 if(!isset($_SESSION['user_id'])) header("Location: login.php");
 
@@ -130,3 +135,4 @@ $products = $productObj->getAll();
 
 </body>
 </html>
+
