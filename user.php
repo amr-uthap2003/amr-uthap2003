e2b15c229ff81202943689d909fced46fbd790d9
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-21 20:22:22 +0530

    Update user.php

--- user.php
+++ user.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 require_once 'db.php';
 
 class User {
@@ -32,3 +37,4 @@ class User {
     }
 }
 ?>
+
