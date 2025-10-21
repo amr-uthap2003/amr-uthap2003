d30e81caa9d8fdc00d4e3fb8964e52355c6d4fa6
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-21 20:19:13 +0530

    Update Order.php

--- Order.php
+++ Order.php
@@ -1,4 +1,10 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<?php
 require_once 'db.php'; // Database connection
 
 class Order {
@@ -73,3 +79,4 @@ class Order {
     }
 }
 ?>
+
