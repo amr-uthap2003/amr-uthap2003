b4b5fdc11aaa1f8261c67e50556be198f60a68d5
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-21 20:21:12 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 session_start();
 
 // Destroy all session data
@@ -8,3 +13,4 @@ session_destroy();
 // Redirect to login or home page
 header("Location: login.php");
 exit();
+
