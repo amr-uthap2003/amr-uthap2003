adf9561c043c684a3ed1484c143c25bb765346a6
amr-uthap2003 <amruthapradeep2003@gmail.com>
2025-10-21 20:21:34 +0530

    Update orders.php

--- orders.php
+++ orders.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 session_start();
 if(!isset($_SESSION['user_id'])) {
     header("Location: login.php");
@@ -54,3 +59,4 @@ $orders = $orderObj->getOrdersByUser($_SESSION['user_id']);
         <?php endforeach; ?>
     <?php endif; ?>
 </div>
+
